f0291af7bd1833d519b8eca47bd2f703ad4106f3
VAbasova <ablerii@ukr.net>
2018-02-12 12:20:57 +0200

    Test that opens all categories

--- navigation.ts
+++ navigation.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 describe('Navigation ',async function() {
     
     it('should open "Upcoming movies" section', async function() {
-        let upcomingMoviesSectionNavigatonButton = $('a[routerlink*="upcoming"]');
+        let upcomingMoviesSectionNavigatonButton = element(by.partialLinkText('Upcoming')); //$('a[routerlink*="upcoming"]');
         let sectionHeader = $('h3.orange-text');
         const UPCOMING_MOVIES_SECTION_LINK = 'https://movies-finder.firebaseapp.com/upcoming';
         const UPCOMING_MOVIES_SECTION_HEDER = 'Up Coming Movies';
@@ -19,7 +19,7 @@ describe('Navigation ',async function() {
     })
 
     it('should open "Popular Series" section', async function(){
-        let popularSeriesSectionNavigatonButton = $('a[routerlink*="popular/series"]');
+        let popularSeriesSectionNavigatonButton = element(by.partialLinkText('Popular Series')); //$('a[routerlink*="popular/series"]');
         let sectionHeader = $('h3.orange-text');
         const POPULAR_SERIES_SECTION_LINK = 'https://movies-finder.firebaseapp.com/popular/series';
         const POPULAR_SERIES_SECTION_HEDER = 'Popular Series';
@@ -32,19 +32,24 @@ describe('Navigation ',async function() {
         expect(await browser.getCurrentUrl()).to.equal(POPULAR_SERIES_SECTION_LINK);
         expect(await sectionHeader.getText()).to.equal(POPULAR_SERIES_SECTION_HEDER);
     })
+})
 
-    it('should open "Action" category', async function(){
-        let actionCategoryMoviesCategory = $('a[href*="/Action"]');
-        let sectionHeader = $('h3.orange-text');
-        const ACTION_CATEGORY_LINK_ENDING = '/Action';
-        const ACTION_CATEGORY_SECTION_HEDER = 'Action';
-        
-        await browser.get('/');
-        await actionCategoryMoviesCategory.click();
-        await browser.sleep(1000);
-
-        //Verify that after click on actionCategoryMoviesCategory url changes to '/Action'
-        expect(await browser.getCurrentUrl()).to.contain(ACTION_CATEGORY_LINK_ENDING);
-        expect(await sectionHeader.getText()).to.equal(ACTION_CATEGORY_SECTION_HEDER);
+describe('Category navigation',async function() {
+    let categories = [ 'Action',  'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy',
+        'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western' ]
+   
+    categories.map(category => {
+        it(`should open ${encodeURI(category)} category`, async function(){
+            console.log(await category); 
+            let moviesCategory = element(by.partialLinkText(category)); //$(`a[href*="/${encodeURI(category)}"]`);
+            let sectionHeader = $('h3.orange-text');
+             
+            await browser.get('/');
+            await moviesCategory.click();
+            await browser.sleep(1000);
+    
+            expect(await browser.getCurrentUrl()).to.contain(encodeURI(category));
+            expect(await sectionHeader.getText()).to.equal(category);
+        })
     })
 })
